4b22a2ac1dcd267dd6942212982bb1c91cf4b702
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-18 15:34:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedWagon::Application.config.secret_key_base = '2269ebd39859bec70e409fddb5ddac68127ea13bbaf86ed9ab337f8a30d3c35b88bdc567f7f84d7a9b3682ab7015eb6e88bb2948f9817683c478e618c19c9c61'
