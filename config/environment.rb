2f5106c1756c1e0a389e6794febcbb9c24ca1e88
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-18 18:28:56 -0500

    update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 RedWagon::Application.initialize!
+
+provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+
