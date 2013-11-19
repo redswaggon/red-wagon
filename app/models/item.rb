ca67e7f37b4f2ebe760460b93d65714f921c6280
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-19 13:34:02 -0500

    photo upload functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
   has_attached_file :photo, styles: {
     thumb: '100x100>',
     square: '200x200#',
-    medium: '300x300>'
-    default_url: "assets/images/missing.png"
+    medium: '300x300>',
+    default_url: 'assets/images/missing.png'
   }
 end
