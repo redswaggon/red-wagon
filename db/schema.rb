79db8f413197ffdaa06728ddfa8601d70e438fe0
Daniel Chang <daniel.chang85@gmail.com>
2013-11-19 13:40:02 -0500

    created user form and neighborhood seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118215353) do
+ActiveRecord::Schema.define(version: 20131119163947) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20131118215353) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+NEIGHBORHOODS = ["Midtown", "Downtown", "Uptown", "Brooklyn", "Queens"]
+
+NEIGHBORHOODS.each do |neighborhood|
+  Neighborhood.create(name: neighborhood)
+end
+
+# MANHATTAN = [ "Harlem", "Inwood", "Morningside Heights", "SpaHa", "Midtown East", "Midtown West", "Upper East Side", "Upper West Side", "Hell's Kitchen", "Murray Hill", "Gramercy", "Union Square", "Meatpacking District", "Chelsea", "Greenwich Village", "East Village", "West Village", "NoHo", "SoHo", "TriBeCa", "Downtown", "Chinatown", "Lower East Side", "Alphabet City"]
+
+# BROOKLYN = [ "Downtown Brooklyn", "Williamsburg", "Dumbo", "Greenpoint", "Bushwick", "Canarsie", "East New York", "Bedford Stuyvesant", "Clinton Hill", "Fort Greene", "Brooklyn Heights", "Gowanus", "Park Slope", "Carroll Gardens", "Boerum Hill", "Red Hook", "Prospect Heights", "Crown Heights", "Prospect Lefferts Gardens", "Windsor Terrace", "Kensington", "Bensonhurst", "Sheepshead Bay", "Coney Island", "Flatbush", "Brownsville", "Navy Yard", "Bay Ridge", "Vinegar Hill", "Sunset Park", "Fort Hamilton", "Gravesand", "Midwood", "East Flatbush", "Flatlands", "Marine Park", "Cypress Hills", "Cobble Hill"]
+
+# NEIGHBORHOODS = {'manhattan': MANHATTAN, 'brooklyn': BROOKLYN}
+
+# NEIGHBORHOODS.each_value.each do |neighborhood|
+#   Neighborhood.create(name: neighborhood)
+# end
