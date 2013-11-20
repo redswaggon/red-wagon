0825bc7828adb76d5245539924c81ea8b6a55308
Daniel Chang <daniel.chang85@gmail.com>
2013-11-19 23:05:51 -0500

    started jquery for add photo button

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ RedWagon::Application.routes.draw do
   get '/users/:username/items' => 'items#edit', as: 'edit_user_items'
   get '/users/:username/items/:id' => 'items#show', as: 'user_item'
   delete '/users/:username/items/:id' => 'items#destroy'
-  post '/users/:username/items/:id' => 'items#create'
   patch '/users/:username/items/:id' => 'items#update'
   put '/users/:username/items/:id' => 'items#update'
+  post '/users/:username/items' => 'items#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
