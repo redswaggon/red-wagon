7eefa16964044cfca8e39f400af3cbb2dd333b00
Daniel Chang <daniel.chang85@gmail.com>
2013-11-21 21:53:11 -0500

    Like almost works.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,13 @@
 class LikesController < ApplicationController
   def create
+    if Like.create(user_id: params[:user], liked_user: params[:stranger])
+      msg = "Liked!"
+    else
+      msg = "failed :("
+    end
+    response = {
+      message: msg
+    }
+    render :json response
   end
 end
