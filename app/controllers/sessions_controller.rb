b48a7531b056ddae27fabca9cea5dac428aae05a
Daniel Chang <daniel.chang85@gmail.com>
2013-11-26 14:19:20 -0500

    minor fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 # OAUTH FACEBOOK
   def create
     auth = request.env["omniauth.auth"]
-    user = (User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth))
+    user = (User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.from_omniauth(auth))
     session[:user_id] = user.id
     redirect_to user_path(user), notice: "Signed in!"
   end
