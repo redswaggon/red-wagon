d7ef921516dcb7801a6f5ff3aee8639197950f0f
Daniel Chang <daniel.chang85@gmail.com>
2013-12-10 13:48:20 -0500

    setup for specs

--- Gemfile
+++ Gemfile
@@ -10,6 +10,19 @@ group :development do
   gem 'pry'
 end
 
+group :development, :test do
+  gem "rspec-rails"
+  gem "factory_girl_rails"
+end
+
+group :test do
+  gem "faker"
+  gem "capybara"
+  gem "database_cleaner"
+  gem "launchy"
+  gem "selenium-webdriver"
+end
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(:default, Rails.env)
 
 module RedWagon
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: true,
+        helper_specs: false,
+        routing_specs: true,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+     end
     # config.assets.path << "#{Rails}/vendor/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
