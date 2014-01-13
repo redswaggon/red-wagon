3d1b6983571084216297347115172cbed8c50a60
danielchangNYC <daniel.chang85@gmail.com>
2014-01-13 15:56:52 -0500

    moved chat creation and finding to model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,4 +5,13 @@ class Chat < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :chatted_user_id, presence: true
+
+  def create_or_find(curr_user, liked_user)
+    if !Chat.where("user_id = ? AND chatted_user_id = ?", curr_user.id, liked_user.id).empty?
+      Chat.where("user_id = ? AND chatted_user_id = ?", curr_user.id, liked_user.id).first
+    elsif !Chat.where("chatted_user_id = ? AND user_id = ?", curr_user.id, liked_user.id).empty?
+      Chat.where("chatted_user_id = ? AND user_id = ?", curr_user.id, liked_user.id).first
+    else
+      Chat.create(user_id: curr_user.id, chatted_user_id: liked_user.id)
+    end 
 end
