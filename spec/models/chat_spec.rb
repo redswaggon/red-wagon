673d6aee17673a1f05c88a13bc6e96978436c1ee
danielchangNYC <daniel.chang85@gmail.com>
2014-01-13 19:51:38 -0500

    chat tests complete

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -2,16 +2,13 @@ require 'spec_helper'
 
 describe Chat do
   before :each do
-    @mike = create(:user, username: "mike")
-    @dan = create(:user, username: "dan")
-    @chat = Chat.create(user_id: @dan.id, chatted_user_id: @mike.id)
-    create(:message, chat_id: @chat.id, to: @mike.username, from: @dan.username)
-    create(:message, chat_id: @chat.id, to: @dan.username, from: @mike.username)
+    @mike = create(:user)
+    @dan = create(:user)
   end
 
   context "initialization" do
     it "is valid with a user and a chatted user" do
-      expect(@chat).to be_valid
+      expect(Chat.create(user_id: @dan.id, chatted_user_id: @mike.id)).to be_valid
     end
 
     it "is invalid without a chatted user" do
@@ -25,25 +22,25 @@ describe Chat do
 
   context "Chat#generate" do
     it "creates a chat if one does not exist" do
-      bob = create(:user, username: "bob")
-      new_chat = Chat.generate(@dan, bob)
-      expect(Chat.where("chatted_user_id = ?", bob.id).first).to eq(new_chat)
-    end
-
-    it "creates a chat if one does not exist" do
-
+      new_chat = Chat.generate(@dan, @mike)
+      expect(Chat.where("chatted_user_id = ?", @mike.id).first).to eq(new_chat)
     end
 
     context "chat already created" do 
       it "finds a chat if one has been created from user to chatted user" do
-
+        Chat.create user_id: @dan.id, chatted_user_id: @mike.id
+        num_of_chats = Chat.all.count
+        Chat.generate(@dan, @mike)
+        expect(Chat.all.count).to eq(num_of_chats)
       end
 
       it "find a chat if one has been created from chatted user to user" do
-
+        Chat.create user_id: @mike.id, chatted_user_id: @dan.id
+        num_of_chats = Chat.all.count
+        Chat.generate(@dan, @mike)
+        expect(Chat.all.count).to eq(num_of_chats)
       end
     end
-
   end
 
 end
