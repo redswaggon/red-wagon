4719947d80a8f3531923d8234f5f8c457369d532
Daniel Chang <daniel.chang85@gmail.com>
2014-01-17 11:05:23 -0500

    wrote sessions controller new spec (front page). others are omniauth specific

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe SessionsController do
   describe "GET #new" do
+    it "renders the :new view" do
+      get :new
+      expect(response).to render_template :new
+    end
   end
-
-  
 end
