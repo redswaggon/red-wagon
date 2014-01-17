d91ebf1f412c8a9a1c65ef3b277f5859ae5c699c
Daniel Chang <daniel.chang85@gmail.com>
2014-01-17 14:53:22 -0500

    I've got 31 problems and a redirect is one

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
-#   get '/users/random_wagon_nearby/:username' => 'users#random_wagon_nearby', as: 'random_wagon_nearby'
-
 describe UsersController do
+  before :each do
+    @user = create(:user)
+    session[:user_id] = @user.id
+  end
+
   describe "GET #index" do
     context "not logged in" do
       it "redirects to sessions#new (root)" do
+        session[:user_id] = nil
         get :index
         expect(response).to redirect_to root_url
       end
@@ -13,10 +17,8 @@ describe UsersController do
 
     context "logged in" do
       it "redirects to users#show" do
-        user = create(:user)
-        session[:user_id] = user.id
         get :index
-        expect(response).to redirect_to user_path(user)
+        expect(response).to redirect_to user_path(@user)
       end
     end
   end
@@ -35,32 +37,24 @@ describe UsersController do
 
   describe "GET #edit" do
     it "assigns the current user to @user" do
-      user = create(:user)
-      session[:user_id] = user.id
-      get :edit, username: user
-      expect(assigns(:user)).to eq user 
+      get :edit, username: @user
+      expect(assigns(:user)).to eq @user 
     end
 
     it "renders the :edit template" do
-      user = create(:user)
-      session[:user_id] = user.id
-      get :edit, username: user
+      get :edit, username: @user
       expect(response).to render_template :edit 
     end
   end
 
   describe "GET #show" do
     it "assigns the current user to @user" do
-      user = create(:user)
-      session[:user_id] = user.id
-      get :show, username: user
-      expect(assigns(:user)).to eq user 
+      get :show, username: @user
+      expect(assigns(:user)).to eq @user 
     end
 
     it "renders the :show template" do
-      user = create(:user)
-      session[:user_id] = user.id
-      get :show, username: user
+      get :show, username: @user
       expect(response).to render_template :show 
     end
   end
@@ -68,13 +62,11 @@ describe UsersController do
   describe "GET #random_wagon_nearby" do
     context "there are non-empty wagons in user's neighborhood" do
       before :each do
-        @user = create(:user)
         @user2 = create(:user)
         @bk = create(:neighborhood)
         @user.neighborhoods << @bk
         @user2.neighborhoods << @bk
         @user2.items.create(photo_file_name: "test_file")
-        session[:user_id] = @user.id
       end
 
       it "assigns another user to @stranger" do
@@ -90,22 +82,60 @@ describe UsersController do
 
     context "there are no filled filled wagons in user's neighborhood" do
       it "renders the :error2 template" do
-        user = create(:user)
-        user.neighborhoods << @bk
-        session[:user_id] = user.id
-        get :random_wagon_nearby, username: user
+        @bk = create(:neighborhood)
+        @user.neighborhoods << @bk
+        get :random_wagon_nearby, username: @user
         expect(response).to render_template :error2
       end
     end
   end
 
-  describe "POST #create" do
+  # describe "POST #create" do
+  #   before :each do
+  #     @bk = create(:neighborhood)
+  #   end
 
-  end
+  #   it "saves the new user to the database" do
+  #     expect{
+  #       post :create, user: attributes_for(:user), neighborhood: {neighborhoods: 1}
+  #     }.to change(User, :count).by(1)
+  #   end
+
+  #   it "redirects to users#show"
+  # end
 
   describe "PUT #update" do
+    before :each do
+      @bw = create(:neighborhood)
+    end
+    
+    it "located the requested user" do
+      put :update, username: @user, user: attributes_for(:user, username: "Bobby"), neighborhood: { neighborhoods: @bw.id }
+      expect(assigns(:user)).to eq @user
+    end
+
+    it "changes the user's attributes" do
+      put :update, username: @user, user: attributes_for(:user, username: "Bobby"), neighborhood: { neighborhoods: @bw.id }
+      @user.reload
+      expect(@user.username).to eq "Bobby"
+    end
+
+    it "redirects to the updated contact's #show path" do
+      put :update, username: @user, user: attributes_for(:user, username: "Bobby"), neighborhood: { neighborhoods: @bw.id }
+      expect(response).to redirect_to user_path(@user)
+    end
   end
 
   describe "DELETE #destroy" do
+    it "deletes the user" do
+      expect{
+        delete :destroy, username: @user
+      }.to change(User, :count).by(-1)
+    end
+
+    it "redirects to root url sessions#new" do
+      delete :destroy, username: @user.id
+      expect(response).to redirect_to root_url
+    end
   end
 end
