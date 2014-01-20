97bf0fb2dabf14bb46813bf83155d2b11b6f7a18
Daniel Chang <daniel.chang85@gmail.com>
2014-01-19 22:20:23 -0500

    refactored with shoulda and for readability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,37 +4,55 @@ describe User do
   let(:user) { create(:user, username: "bob") }
   let(:user2) { create(:user) }
 
-  context "initializes unique users" do
-    it "is valid with a unique username" do
-      expect(user).to be_valid
+  describe "associates with likes, messages, items and neighborhoods" do
+    it { should have_many(:likes) }
+    it { should have_many(:liked_users).through(:likes) }
+    it { should have_many(:items) }
+    it { should have_many(:messages) }
+    it { should have_and_belong_to_many(:neighborhoods) }
+  end
+
+  describe "initializes unique users" do
+    context "when username is unique" do
+      it { should validate_uniqueness_of(:username).case_insensitive }
     end
 
-    it "is invalid if the username already exists, no matter what casing" do
-      user
-      expect(build(:user, username: "Bob")).to have(1).errors_on(:username)
+    context "when username already exists" do
+      it "returns an error" do
+        user
+        expect(build(:user, username: "Bob")).to have(1).errors_on(:username)
+      end
     end
   end
 
-  context "handles usernames and mutually liked users" do  
-    it "returns the user's username when calling to_param" do
-      expect(user.to_param).to eq(user.username)
+  describe "handles usernames and mutually liked users" do  
+    context "when calling to_param" do
+      it "returns the user's username" do
+        expect(user.to_param).to eq(user.username)
+      end
     end
 
-    it "returns mutually liked users when calling mutually liked users" do
-      user.likes.create(liked_user_id: user2.id)
-      user2.likes.create(liked_user_id: user.id)
-      expect(user2.mutually_liked_users).to include user
+    context "when calling mutually liked users" do
+      it "returns mutually liked users" do
+        user.likes.create(liked_user_id: user2.id)
+        user2.likes.create(liked_user_id: user.id)
+        expect(user2.mutually_liked_users).to include user
+      end
     end
   end
 
-  context "determines if wagon is empty" do
-    it "wagon is empty" do
-      expect(user.empty_wagon?).to be_true
+  describe "#empty_wagon?" do
+    context "when wagon is empty" do
+      it "returns true" do
+        expect(user.empty_wagon?).to be_true
+      end
     end
 
-    it "wagon has at least one item" do
-      user2.items.create(photo_file_name: "test_file")
-      expect(user2.empty_wagon?).to be_false
+    context "when wagon has at least one item" do
+      it "returns false" do
+        user2.items.create(photo_file_name: "test_file")
+        expect(user2.empty_wagon?).to be_false
+      end
     end
   end
 
