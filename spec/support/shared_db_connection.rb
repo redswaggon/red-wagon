625043b0093979e3e7e3f5c61c3d4baf8894c221
Daniel Chang <daniel.chang85@gmail.com>
2014-02-22 16:32:43 -0500

    small mod

--- spec/support/shared_db_connection.rb
+++ spec/support/shared_db_connection.rb
@@ -1,3 +1,5 @@
+# Due to how Selenium handles database transactions, we need to share data state across the Selenium web server and the test code itself. Without this patch we're apt to get sporadic error messages resulting from tests not properly cleaning up after themselves.
+
 class ActiveRecord::Base
   mattr_accessor :shared_connection
   @@shared_connection = nil
